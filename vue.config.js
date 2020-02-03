d2abff2ff301414866f2d1485af3a55af953d958
AurioPinto <auriopinto@outlook.com>
2020-02-03 15:24:12 +0800

    Project root

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+// vue.config.js
+module.exports = {
+  publicPath: './',
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.png',
+      favicon16: 'favicon.png',
+      appleTouchIcon: 'favicon.png',
+      maskIcon: 'favicon.png',
+      msTileImage: 'favicon.png',
+    },
+  },
+};
